78ab5988c37386cf41a53ff6b4841e1457371491
asd2d <872122@mail.ru>
2024-11-15 11:31:10 +0300

    first route handler created

--- public/index.php
+++ public/index.php
@@ -3,11 +3,17 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+    return $this->get('renderer')->render($response, 'home.phtml');
 });
+
 $app->run();
